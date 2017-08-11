06910dc35d044b67bc63ef8d941ace66ac746794
christopher <cjwright4242@gmail.com>
2017-08-11 12:46:57 -0400

    WIP: write linter

---
+++ scripts/copyright_linter.py
@@ -0,0 +1,11 @@
+
+template_lines = ['##############################################################################',
+                  '#',
+                  '# {{ project_name }}            by Billinge Group',
+                  '#                   Simon J. L. Billinge sb2896@columbia.edu',
+                  '#                   (c) {{ year }} trustees of Columbia University in the City of', '#                        New York.', '#                   All rights reserved', '#', '# File coded by:    {{ authors }}', '#', '# See AUTHORS.txt for a list of people who contributed.', '# See LICENSE.txt for license information.', '#', '##############################################################################']
+
+
+
+def lint_for_copyright(string):
+
