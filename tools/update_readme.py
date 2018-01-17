fde7324287f02f441f6d136af3c0bbb7e36cd8ea
christopher <cjwright4242@gmail.com>
2018-01-17 16:27:31 -0500

    FIX: missing columns

--- tools/update_readme.py
+++ tools/update_readme.py
@@ -11,7 +11,7 @@ Releases, Installers, Specs, and more!
 
 # Build status
 | Repo | Build | Coverage | Health | Version | Downloads|
-|:-------:|:-----:|:--------:|:------:|
+|:-------:|:-----:|:--------:|:------:|:------:|:------:|
 '''
 for b in ['xpdSim', 'streamz_ext', 'xpdtools', 'SHED', 'xpdAn', 'xpdAcq',
           'xpd-shadow']:
