d9570ce870c3c344db94292ff57094d3339a5d68
christopher <cjwright4242@gmail.com>
2018-01-23 20:59:57 -0500

    ENH: add NSLS-II version to build status

--- tools/update_readme.py
+++ tools/update_readme.py
@@ -2,16 +2,17 @@ main_format=dict(
     build='[![Build Status](https://travis-ci.org/xpdAcq/{name}.svg?branch=master)](https://travis-ci.org/xpdAcq/{name})',
     coverage='[![codecov](https://codecov.io/gh/xpdAcq/{name}/branch/master/graph/badge.svg)](https://codecov.io/gh/xpdAcq/{name}) ',
     health='[![Code Health](https://landscape.io/github/xpdAcq/{name}/master/landscape.svg?style=flat)](https://landscape.io/github/xpdAcq/{name}/master)',
-    version='[![Anaconda-Server Badge](https://anaconda.org/conda-forge/{name}/badges/version.svg)](https://anaconda.org/conda-forge/{name})',
+    cf_version='[![Anaconda-Server Badge](https://anaconda.org/conda-forge/{name}/badges/version.svg)](https://anaconda.org/conda-forge/{name})',
+    nsls_version='[![Anaconda-Server Badge](https://anaconda.org/lightsource2-tag/{name}/badges/version.svg)](https://anaconda.org/lightsource2-tag/{name})',
     downloads='[![Anaconda-Server Badge](https://anaconda.org/conda-forge/{name}/badges/downloads.svg)](https://anaconda.org/conda-forge/{name})')
 
-a = '''|{name}|{build}|{coverage}|{health}|{version}|{downloads}|\n'''
+a = '''|{name}|{build}|{coverage}|{health}|{cf_version}|{nsls_version}|{downloads}|\n'''
 c = '''# mission-control
 Releases, Installers, Specs, and more!
 
 # Build status
-| Repo | Build | Coverage | Health | Version | Downloads|
-|:-------:|:-----:|:--------:|:------:|:------:|:------:|
+| Repo | Build | Coverage | Health | CF Version | NSLS Version | Downloads|
+|:-------:|:-----:|:--------:|:------:|:------:|:------:|:------:|
 '''
 for b in ['xpdSim', 'streamz_ext', 'xpdtools', 'SHED', 'xpdAn', 'xpdAcq',
           'xpd-shadow']:
