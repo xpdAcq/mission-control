8a1221ec2f8dac67ac2f4c142b4ce1cffb4dcae6
christopher <cjwright4242@gmail.com>
2018-06-10 17:02:02 -0400

    ENH: add more projects to project roll

--- tools/tag_tracker.py
+++ tools/tag_tracker.py
@@ -8,6 +8,7 @@ def create_repo_dict(repo_path, authors=None,
     l = {}
     r = Repo(repo_path)
     tags = r.tags
+    print(r, len(tags))
     for tag in tags:
         d = dict(title=os.path.split(repo_path)[-1],
                  version=str(tag),
@@ -35,8 +36,10 @@ if __name__ == '__main__':
     a = '/home/christopher/dev/'
     if not os.path.isdir(a):
         a = '/Users/timothyliu/Repo/'
-    rl = [a + b for b in ['xpdAn', 'xpdAcq', 'SHED', 'streamz']]
-    authors_list = [('Christopher J. Wright', 'Chia-Hao Liu',
+    rl = [a + b for b in ['streamz_ext', 'xpdtools', 'xpdAn', 'xpdAcq', 'SHED', 'streamz']]
+    authors_list = [('Christopher J. Wright', 'Simon J. L. Billinge'), 
+                    ('Christopher J. Wright', 'Simon J. L. Billinge'),
+                    ('Christopher J. Wright', 'Chia-Hao Liu',
                      'Simon J. L. Billinge'),
                     ('Chia-Hao Liu', 'Christopher J. Wright',
                      'Simon J. L. Billinge'),
@@ -47,16 +50,17 @@ if __name__ == '__main__':
     s = track_repos(rl, authors_list, ['https://github.com/xpdAcq/',
                                        'https://github.com/xpdAcq/',
                                        'https://github.com/xpdAcq/',
+                                       'https://github.com/xpdAcq/',
+                                       'https://github.com/xpdAcq/',
                                        'https://github.com/mrocklin/'])
     s2 = []
     for k, v in s.items():
         for k2, v2 in v.items():
             s2.append(v2)
-    pprint(s2)
+#    pprint(s2)
     db = BibDatabase()
     db.entries = s2
     writer = BibTexWriter()
-    print(writer.write(db))
     with open('bg-software.bib', 'w') as bibfile:
         bibfile.write(writer.write(db))
 
